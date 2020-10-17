d292ebb9de45765afc7a047c5d4847205f9ec7e0
amarismail522 <56680798+amarismail522@users.noreply.github.com>
2020-10-17 23:24:16 +0500

    update Appbar in header.js
    add logo and input field

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,35 +6,33 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import TextField from '@material-ui/core/TextField';
+import Logotyp_OLX_ from './Logotyp_OLX_.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
+ 
 }));
+
 
 export default function Header() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+    <div className="customNav">
+    <div>
+      <Toolbar>
+        <div className="logo">
+            <img src={Logotyp_OLX_}  alt="logo"/>
+        </div>
+        <div>
+          <input placeholder="Pakistan" className="textfield" />
+        </div>
+      </Toolbar>
+    </div>
     </div>
   );
 }
