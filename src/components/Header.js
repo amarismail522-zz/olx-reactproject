5062841e933b1156749b10769a7050919974aba2
amarismail522 <56680798+amarismail522@users.noreply.github.com>
2020-10-18 12:10:27 +0500

    modify header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -14,29 +16,94 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    
-  },
- 
-}));
 
+  },
+
+}));
 
 export default function Header() {
   const classes = useStyles();
 
   return (
-    <div className="customNav">
-    <div>
-      <Toolbar>
-        <div className="logo">
-            <img src={Logotyp_OLX_}  alt="logo"/>
+    <div className={classes.root}>
+      <Grid container >
+        <div className="customNav">
+          <Toolbar>
+            <Grid item xs={1}>
+              <div className="logo" style={{ display: "inline-flex" }}>
+                <img src={Logotyp_OLX_} alt="logo" />
+              </div>
+            </Grid>
+            <Grid item xs={2}>
+
+              <div className="location-item">
+                <span className="search-span1"><SearchIcon style={{ fontSize: "34px" }} /></span>
+                <input placeholder="Pakistan" className="textfield" />
+                <span className="expandless-icon">
+                  <button className="btn-dropdown">
+                    <ExpandLessIcon style={{ fontSize: "34px" }} />
+                  </button>
+                </span>
+              </div>
+            </Grid>
+            <Grid item xs={5}>
+              <div className="location-item1">
+
+                <input placeholder="Pakistan" className="textfield" />
+
+                <span className="search-span2 "><SearchIcon style={{ fontSize: "34px" }} /></span>
+
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              hi
+        </Grid>
+          </Toolbar>
         </div>
-        <div>
-          <span><SearchIcon /></span>
-          <input placeholder="Pakistan" className="textfield" />
-          <span className="expandless-icon"><ExpandLessIcon /></span>
-        </div>
-      </Toolbar>
-    </div>
+
+      </Grid>
     </div>
   );
 }
+// export default function Header() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className="customNav">
+//       <div>
+//         <Toolbar>
+//           <div className="logo">
+//             <img src={Logotyp_OLX_} alt="logo" />
+//           </div>
+//           <div className="location-item">
+//             <span className="search-span1"><SearchIcon style={{ fontSize: "34px" }} /></span>
+//             <input placeholder="Pakistan" className="textfield" />
+//             <span className="expandless-icon">
+//               <button className="btn-dropdown">
+//                 <ExpandLessIcon style={{ fontSize: "34px" }} />
+//               </button>
+//             </span>
+//           </div>
+//           <div className="location-item">
+//             <span className="search-span1"><SearchIcon style={{ fontSize: "34px" }} /></span>
+//             <input placeholder="Pakistan" className="textfield" />
+//             <span className="expandless-icon">
+//               <button className="btn-dropdown">
+//                 <ExpandLessIcon style={{ fontSize: "34px" }} />
+//               </button>
+//             </span>
+//           </div>
+//           <div className="location-item">
+//             <span className="search-span1"><SearchIcon style={{ fontSize: "34px" }} /></span>
+//             <input placeholder="Pakistan" className="textfield" />
+//             <span className="expandless-icon">
+//               <button className="btn-dropdown">
+//                 <ExpandLessIcon style={{ fontSize: "34px" }} />
+//               </button>
+//             </span>
+//           </div>
+//         </Toolbar>
+//       </div>
+//     </div>
+//   );
+// }
